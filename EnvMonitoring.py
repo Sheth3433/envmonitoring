db3094694890723f51a136cef8413787f430a294
Aarjav Sheth <149706948+Sheth3433@users.noreply.github.com>
2025-07-09 23:21:23 +0530

    Update EnvMonitoring.py

--- EnvMonitoring.py
+++ EnvMonitoring.py
@@ -4,6 +4,7 @@ import os
 import requests
 from PIL import Image
 import matplotlib.pyplot as plt
+from io import BytesIO
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.image import img_to_array
 # -------------------- CONFIG --------------------
